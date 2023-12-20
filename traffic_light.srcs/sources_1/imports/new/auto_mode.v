dc5f6e02a553d859d96e7d8938db0939b85f45bc
La Minh Duc <laminhduc200@gmail.com>
2023-12-20 11:33:50 +0700

    create and add auto mode for traffic light system

---
+++ traffic_light.srcs/sources_1/imports/new/auto_mode.v
@@ -0,0 +1,62 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/19/2023 04:18:55 PM
+// Design Name: 
+// Module Name: auto_mode
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module auto_mode(
+    input clk,
+    input enable,
+    input [6:0]count_red,
+    input [6:0]count_yellow,
+    input [6:0]count_green,
+    output [2:0]first_way,
+    output [2:0]second_way,
+    output [6:0]first_counter,
+    output [6:0]second_counter
+    );
+    
+ parameter RED = 2'b01, YELLOW = 2'b10, GREEN = 2'b11;
+ wire red1, yellow1, green1;
+ wire red2, yellow2, green2;
+ 
+ single traffic_1(.clk(clk), 
+                  .enable(enable), 
+                  .init_state(RED),
+                  .count_red(count_red), 
+                  .count_yellow(count_yellow),
+                  .count_green(count_green),
+                  .red(red1),
+                  .yellow(yellow1),
+                  .green(green1),
+                  .counter(first_counter));
+ single traffic_2(.clk(clk), 
+                  .enable(enable), 
+                  .init_state(GREEN),
+                  .count_red(count_red), 
+                  .count_yellow(count_yellow),
+                  .count_green(count_green),
+                  .red(red2),
+                  .yellow(yellow2),
+                  .green(green2),
+                  .counter(second_counter));               
+   
+   assign first_way = {red1, yellow1, green1};
+   assign second_way = {red2, yellow2, green2};
+endmodule
---
+++ traffic_light.srcs/sources_1/imports/new/single.v
@@ -0,0 +1,80 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/20/2023 12:05:21 AM
+// Design Name: 
+// Module Name: single
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module single(
+    input clk,
+    input enable,
+    input [1:0]init_state,
+    input [6:0]count_red,
+    input [6:0]count_yellow,
+    input [6:0]count_green,
+    output red,
+    output yellow,
+    output green,
+    output reg [6:0]counter
+    );
+    
+    parameter INIT = 2'b00, RED = 2'b01, YELLOW = 2'b10, GREEN = 2'b11;
+    reg [1:0]state = INIT;
+    
+    assign {red, yellow, green} = (4'b1000 >> state) & {7{enable}};
+
+    always @(posedge clk) begin
+        case (state)
+            INIT: begin
+                if (init_state == INIT) state = RED;
+                else state = init_state;
+                
+                case (state)
+                    RED: counter = count_red - 1;
+                    YELLOW: counter = count_yellow - 1;
+                    GREEN: counter = count_green - 1;
+                endcase
+            end
+            RED: begin                
+                if (counter == 0) begin
+                    counter = count_green;
+                    state = GREEN;
+                end
+                counter = counter - 1;
+            end
+            YELLOW: begin
+                if (counter == 0) begin
+                    counter = count_red;
+                    state = RED;
+                end
+                counter = counter - 1;
+            end
+            
+            GREEN: begin
+                if (counter == 0) begin
+                    counter = count_yellow;
+                    state = YELLOW;
+                end
+                counter = counter - 1;
+            end
+        endcase
+        
+        counter = counter & {7{enable}};
+        state = state & {2{enable}};
+    end
+endmodule
